2263f5822002d0db32dae195c66139d9803f5e26
isBeo <dammybeo@gmail.com>
2024-11-28 03:13:44 -0800

    v.1

--- components/Video.tsx
+++ components/Video.tsx
@@ -3,7 +3,7 @@
 const Video = () => {
   return (
     <div  >
-    <iframe width="80%" className="mx-auto h-[50vh]" height="315" src="https://www.youtube.com/embed/kTswln1kNmc?si=8-78Z4uQfLzjxe0A" title="YouTube video player"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  ></iframe>
+    <iframe width="80%" className="mx-auto h-[50vh]" height="315" src="https://www.youtube.com/embed/kTswln1kNmc?si=8-78Z4uQfLzjxe0A" title="YouTube video player"allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
 
 
     </div>
